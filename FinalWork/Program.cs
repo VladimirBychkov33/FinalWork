2347863d2be93d0719c5869cac634c12e84303e7
Vladimir <grandtwix33@mail.ru>
2022-12-03 14:52:23 +0500

    add Programming code

---
+++ FinalWork/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+string[] array = { "GeekBrains", "hello", "12.2022", "555" };
+Console.WriteLine(String.Join(' ', array)); 
+Console.WriteLine();
+string temp = String.Empty;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i].Length < 4) temp = temp + array[i] + " ";          
+}
+string[] result = temp.Split(' ');
+Console.WriteLine(String.Join(' ', result));
