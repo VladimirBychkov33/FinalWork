e9ad50fbbcb0f57599428b527ac8f02a0f8b1e6c
Vladimir <grandtwix33@mail.ru>
2022-12-03 15:32:05 +0500

    add final_1 README

--- FinalWork/Program.cs
+++ FinalWork/Program.cs
@@ -1,11 +1,18 @@
 ﻿Console.Clear();
-string[] array = { "GeekBrains", "hello", "12.2022", "555" };
-Console.WriteLine(String.Join(' ', array)); 
+string[] array = { "GeekBrains", "hel", "12.2022", "555" };
+Console.WriteLine(String.Join(' ', array));
 Console.WriteLine();
-string temp = String.Empty;
-for (int i = 0; i < array.Length; i++)
+string[] final = NewString(array);
+Console.WriteLine(String.Join(' ', final));
+
+string[] NewString(string[] massiv)
 {
-    if (array[i].Length < 4) temp = temp + array[i] + " ";          
+    string[] result;
+    string temp = String.Empty;
+    for (int i = 0; i < massiv.Length; i++)
+    {
+        if (massiv[i].Length < 4) temp = temp + massiv[i] + " ";
+    }
+    result = temp.Split(' ');
+    return result;  
 }
-string[] result = temp.Split(' ');
-Console.WriteLine(String.Join(' ', result));
